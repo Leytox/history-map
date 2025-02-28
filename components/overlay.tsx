8e5544b7545aab267430969affdd76280888a596
Ilya Devder <iladevder@gmail.com>
2025-03-01 00:50:22 +0200

    refactor: enhance overlay component with responsive menu and improved location handling

--- components/overlay.tsx
+++ components/overlay.tsx
@@ -1,13 +1,15 @@
 import {
   Compass,
-  GithubIcon,
   GlobeIcon,
   HomeIcon,
+  InfoIcon,
   LoaderIcon,
   MapIcon,
   MapPin,
+  MenuIcon,
   SearchIcon,
   SettingsIcon,
+  XIcon,
   ZoomIn,
   ZoomOut,
 } from "lucide-react";
@@ -15,7 +17,7 @@ import { Button } from "./ui/button";
 import ThemeSwitcher from "./theme-switcher";
 import SettingsDialog from "./settings";
 import { Card, CardContent } from "./ui/card";
-import { useLocalStorage } from "@uidotdev/usehooks";
+import { useLocalStorage, useWindowSize } from "@uidotdev/usehooks";
 import { useCallback, useState } from "react";
 import Search from "./search";
 import { ProjectionSpecification } from "maplibre-gl";
@@ -40,17 +42,17 @@ export default function Overlay({
     true
   );
   const [projection, setProjection] = useLocalStorage<
-    ProjectionSpecification["type"]
+    ProjectionSpecification | "mercator" | "globe" | undefined
   >("projection", "mercator");
   const [isSearchOpen, setIsSearchOpen] = useState<boolean>(false);
   const [isLocating, setIsLocating] = useState<boolean>(false);
   const [isSettingsOpen, setIsSettingsOpen] = useState<boolean>(false);
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [isSearching, setIsSearching] = useState<boolean>(false);
-
+  const [menuShown, setMenuShown] = useState<boolean>(false);
+  const size = useWindowSize();
   const showCurrentLocation = async () => {
     setIsLocating(true);
-    let latitude, longitude;
     try {
       const position = await new Promise<GeolocationPosition>(
         (resolve, reject) => {
@@ -58,8 +60,8 @@ export default function Overlay({
         }
       );
 
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
 
       mapRef.current?.flyTo({
         center: [longitude, latitude],
@@ -69,7 +71,6 @@ export default function Overlay({
       toast.success("Location found");
     } catch (error) {
       console.error("Geolocation error:", error);
-      toast.error("Unable to use browser location service");
       try {
         const geoData = await getGeoDataExternal();
 
@@ -86,16 +87,6 @@ export default function Overlay({
     } finally {
       setIsLocating(false);
     }
-  };
-
-  const handleZoomIn = () => {
-    if (!mapRef.current) return;
-    mapRef.current.zoomIn();
-  };
-
-  const handleZoomOut = () => {
-    if (!mapRef.current) return;
-    mapRef.current.zoomOut();
   };
 
   const resetView = useCallback(() => {
@@ -130,63 +121,129 @@ export default function Overlay({
 
   return (
     <>
-      {isSearchOpen && (
-        <Search
-          isOpen={isSearchOpen}
-          setIsOpen={setIsSearchOpen}
-          handleSearch={handleSearch}
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
-          isSearching={isSearching}
-        />
-      )}
-      <div className="absolute top-2 right-2 flex flex-col gap-2">
+      {size.width! <= 768 && !menuShown ? (
         <Button
-          onClick={() =>
-            setProjection(projection === "mercator" ? "globe" : "mercator")
-          }
-          title="Switch projection"
+          className="absolute top-2 right-2"
           type="button"
+          onClick={() => setMenuShown(true)}
+          title="Expand Menu"
           size={"icon"}
         >
-          {projection === "mercator" ? <GlobeIcon /> : <MapIcon />}
+          <MenuIcon size={16} />
         </Button>
-        <ThemeSwitcher />
-        <Button
-          onClick={showCurrentLocation}
-          title="Find my location"
-          type="button"
-          size={"icon"}
-          disabled={isLocating}
-        >
-          {isLocating ? (
-            <LoaderIcon size={16} className="animate-spin" />
-          ) : (
-            <MapPin />
+      ) : (
+        <>
+          {isSearchOpen && (
+            <Search
+              isOpen={isSearchOpen}
+              setIsOpen={setIsSearchOpen}
+              handleSearch={handleSearch}
+              searchQuery={searchQuery}
+              setSearchQuery={setSearchQuery}
+              isSearching={isSearching}
+            />
           )}
-        </Button>
-        <Button
-          size="icon"
-          title="Search"
-          type="button"
-          onClick={() => setIsSearchOpen(true)}
-        >
-          <SearchIcon />
-        </Button>
-        <Button
-          size="icon"
-          title="Settings"
-          type="button"
-          onClick={() => setIsSettingsOpen(true)}
-        >
-          <SettingsIcon />
-        </Button>
-        <a target="_blank" href="https://github.com/Leytox/history-map">
-          <Button title="This project on Github" type="button" size={"icon"}>
-            <GithubIcon />
-          </Button>
-        </a>
-      </div>
+          <div className="absolute top-2 right-2 flex flex-col gap-2">
+            <Button
+              onClick={() =>
+                setProjection(projection === "mercator" ? "globe" : "mercator")
+              }
+              title="Switch projection"
+              type="button"
+              size={"icon"}
+            >
+              {projection === "mercator" ? <GlobeIcon /> : <MapIcon />}
+            </Button>
+            <ThemeSwitcher />
+            <Button
+              onClick={showCurrentLocation}
+              title="Find my location"
+              type="button"
+              size={"icon"}
+              disabled={isLocating}
+            >
+              {isLocating ? (
+                <LoaderIcon size={16} className="animate-spin" />
+              ) : (
+                <MapPin />
+              )}
+            </Button>
+            <Button
+              size="icon"
+              title="Search"
+              type="button"
+              onClick={() => setIsSearchOpen(true)}
+            >
+              <SearchIcon />
+            </Button>
+            <Button
+              size="icon"
+              title="Settings"
+              type="button"
+              onClick={() => setIsSettingsOpen(true)}
+            >
+              <SettingsIcon />
+            </Button>
+            <a target="_blank" href="https://github.com/Leytox/history-map">
+              <Button
+                title="This project on Github"
+                type="button"
+                size={"icon"}
+              >
+                <InfoIcon />
+              </Button>
+            </a>
+
+            {showAdditionalButtons && (
+              <div className={"left-2 flex flex-col gap-2"}>
+                <Button
+                  onClick={resetView}
+                  title="Reset view"
+                  type="button"
+                  size={"icon"}
+                  className="shadow-lg"
+                >
+                  <HomeIcon size={18} />
+                </Button>
+                <Button
+                  onClick={() => mapRef?.current?.zoomIn()}
+                  title="Zoom in"
+                  type="button"
+                  size={"icon"}
+                  className="shadow-lg"
+                >
+                  <ZoomIn size={18} />
+                </Button>
+                <Button
+                  onClick={() => mapRef?.current?.zoomOut()}
+                  title="Zoom out"
+                  type="button"
+                  size={"icon"}
+                  className="shadow-lg"
+                >
+                  <ZoomOut size={18} />
+                </Button>
+              </div>
+            )}
+
+            {showZoomLevel && (
+              <Card className="border-0 py-1 px-2.5 bg-foreground text-background">
+                <p className="text-xs">{zoom.toFixed(1)}</p>
+              </Card>
+            )}
+            {menuShown && size.width! <= 768 && (
+              <Button
+                type="button"
+                onClick={() => setMenuShown(false)}
+                title="Expand Menu"
+                size={"icon"}
+              >
+                <XIcon size={16} />
+              </Button>
+            )}
+          </div>
+        </>
+      )}
 
       {showCoordinates && (
         <Card className="border-0 absolute top-2 left-2 bg-foreground items-center text-background min-w-[160px] max-w-[160px] p-1">
@@ -199,50 +256,6 @@ export default function Overlay({
             }`}</span>
           </CardContent>
         </Card>
-      )}
-
-      {showAdditionalButtons && (
-        <div
-          className={`absolute ${
-            showZoomLevel ? "bottom-10" : "bottom-2"
-          } left-2 flex flex-col gap-2`}
-        >
-          <Button
-            onClick={resetView}
-            title="Reset view"
-            type="button"
-            size={"icon"}
-            className="shadow-lg"
-          >
-            <HomeIcon size={18} />
-          </Button>
-          <Button
-            onClick={handleZoomIn}
-            title="Zoom in"
-            type="button"
-            size={"icon"}
-            className="shadow-lg"
-          >
-            <ZoomIn size={18} />
-          </Button>
-          <Button
-            onClick={handleZoomOut}
-            title="Zoom out"
-            type="button"
-            size={"icon"}
-            className="shadow-lg"
-          >
-            <ZoomOut size={18} />
-          </Button>
-        </div>
-      )}
-
-      {showZoomLevel && (
-        <div className="absolute bottom-2 left-2" title="Zoom level">
-          <Card className="border-0 py-1 px-2.5 bg-foreground text-background">
-            <p className="text-xs">{zoom.toFixed(1)}</p>
-          </Card>
-        </div>
       )}
       <SettingsDialog isOpen={isSettingsOpen} setIsOpen={setIsSettingsOpen} />
     </>
